8cd3a23e7c9a279e41019f242a5c344284f621b6
beatsbydan <danielonyeachonam2020@gmail.com>
2022-07-05 15:55:25 -0700

    second task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - to print alphabets
+ *
+ * Return: nothing
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+_putchar('\n');
+}
