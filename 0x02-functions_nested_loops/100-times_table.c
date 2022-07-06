baa35ed5877e4f5cd0b06a144a7dfa5a19c190df
beatsbydan <danielonyeachonam2020@gmail.com>
2022-07-06 08:01:34 -0700

    a 1

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_times_table - prints for a given number
+ * @n: argument
+ * Return: times table
+ */
+void print_times_table(int n)
+{
+	int r;
+	int c;
+	int d;
+
+	for (r = 0; c <= n; r++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (c = 1; c <= 9; c++)
+		{
+			d = (r * c);
+			if ((d / 10) > 0)
+			{
+				_putchar((d / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			if (c < n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if (c > 15 || c < 0)
+			{
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+}
