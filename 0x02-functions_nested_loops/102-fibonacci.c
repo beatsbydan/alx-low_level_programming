0c4570bf8d5d3d2ee88fac62fa64931af097b8bc
beatsbydan <danielonyeachonam2020@gmail.com>
2022-07-06 12:23:53 -0700

    a3

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sequence
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	int _num = 1;
+	int num_1 = 2;
+	int new_num;
+	int total = 50;
+
+	printf("%d, %d, ", _num, num_1);
+	for (n = 0; n < 50; n++)
+	{
+		new_num = _num + num_1;
+		_num = num_1;
+		num_1 = new_num;
+		printf("%d", new_num);
+		if (n != total)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
