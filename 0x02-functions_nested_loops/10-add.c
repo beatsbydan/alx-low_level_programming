1e3d69125a2918f27c0c9dc98dd7426cd0129f7a
beatsbydan <danielonyeachonam2020@gmail.com>
2022-07-06 04:53:37 -0700

    11th task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two digits together
+ * @a: first argument
+ * @b: second argument
+ * Return: 0
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	_putchar(result);
+	_putchar('\n');
+	return (0);
+}
