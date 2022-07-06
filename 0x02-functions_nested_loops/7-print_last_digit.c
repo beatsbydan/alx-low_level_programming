a0329237d7b87ac9fb81b9de8c88e8a2e9f4a369
beatsbydan <danielonyeachonam2020@gmail.com>
2022-07-06 03:23:08 -0700

    8th task

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ * @c: argument to be passed
+ * Return: 0
+ */
+int print_last_digit(int c)
+{
+	if (c > 0 || c == 0)
+	{
+		_putchar (c % 10 + 0);
+		return (c % 10);
+	}
+	else
+	{
+		c = c * -1;
+		_putchar (c % 10 + 0);
+		return (c % 10);
+	}
+}
