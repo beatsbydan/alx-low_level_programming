8739fb8c89941c73e7c65cf019e657b4d4a58c58
beatsbydan <danielonyeachonam2020@gmail.com>
2022-07-06 11:06:03 -0700

    a2

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints sum
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int _num;
+	int _sum;
+
+	for (_num = 0; _num <= 1024; _num++)
+	{
+		if (_num % 3 == 0 || _num % 5 == 0)
+		{
+			_sum += _num;
+		}
+	}
+	printf("%d", _sum);
+	printf("\n");
+	return(0);
+}
