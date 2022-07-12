062933ec6498efabf4e698d5edcd3fb400ad5831
beatsbydan <danielonyeachonam2020@gmail.com>
2022-07-12 13:53:10 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen - prints the length
+ * @*s: pointer for s
+ * @s: variable
+ * Return: string
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
+
