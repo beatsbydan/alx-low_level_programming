034a3aa1a073a2737ac51c5b1d55f38071892470
beatsbydan <danielonyeachonam2020@gmail.com>
2022-07-14 07:38:51 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - compares 2 strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int reti = strcmp(s1, s2);
+	int a = -15;
+	int b = 15;
+	int c = 0;
+
+	if(reti < 0)
+	{
+	       	return (a);
+	} 
+	else if(reti > 0) 
+	{
+		return (b);
+	}
+	else
+	{
+		return (c);
+	}
+	return (0);
+}
