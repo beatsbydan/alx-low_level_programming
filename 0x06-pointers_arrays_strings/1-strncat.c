955a09fe6fc1623a49b4949678e243019ff76bb6
beatsbydan <danielonyeachonam2020@gmail.com>
2022-07-14 06:24:24 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy - copies a string
+ * @dest: destination
+ * @src: source
+ * @n: argument
+ * Return: always 0
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+	return(dest);
+}
