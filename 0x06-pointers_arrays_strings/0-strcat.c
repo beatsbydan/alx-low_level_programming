9b85be98f03bddec0e632c87bdefa873b8252022
beatsbydan <danielonyeachonam2020@gmail.com>
2022-07-14 05:53:18 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - concatenates
+ * @dest: string 1
+ * @src: string 2
+ * Return: 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return (0);
+	_putchar('\n');
+}
